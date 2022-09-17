559f26fef5108861535d14d40098b20e48f3279e
siavash sattari <siavash.sattari.75@gmail.com>
2022-09-17 11:38:01 -0700

    FeedbackStats component

--- resource-a/src/App.js
+++ resource-a/src/App.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 
 import Header from './components/Header';
 import FeedbackList from './components/FeedbackList';
+import FeedbackStats from './components/FeedbackStats';
 import FeedbackData from './data/FeedbackData';
 
 // ==============================================================
@@ -19,6 +20,7 @@ const App = () => {
     <>
       <Header />
       <div className='container'>
+        <FeedbackStats feedback={feedback} />
         <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
     </>
