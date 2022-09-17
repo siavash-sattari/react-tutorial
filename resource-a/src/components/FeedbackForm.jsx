963f5b21bd9d2c73adf72a148806c8270a313b09
siavash sattari <siavash.sattari.75@gmail.com>
2022-09-17 12:20:50 -0700

    RatingSelect component

--- resource-a/src/components/FeedbackForm.jsx
+++ resource-a/src/components/FeedbackForm.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
+import RatingSelect from './RatingSelect';
 import Button from './shared/Button';
 import Card from './shared/Card';
 
 const FeedbackForm = () => {
   const [text, setText] = useState('');
+  const [rating, setRating] = useState(10);
   const [btnDisabled, setBtnDisabled] = useState(true);
   const [message, setMessage] = useState('');
 
@@ -25,6 +27,7 @@ const FeedbackForm = () => {
     <Card>
       <form>
         <h2>How would you rate your service with us?</h2>
+        <RatingSelect select={rating => setRating(rating)} />
         <div className='input-group'>
           <input type='text' placeholder='Write a review' value={text} onChange={handleTextChange} />
           <Button type='submit' isDisabled={btnDisabled}>
