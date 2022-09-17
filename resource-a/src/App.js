35b1341d0229ad7900dc6319db334a201a061ead
siavash sattari <siavash.sattari.75@gmail.com>
2022-09-17 07:39:54 -0700

    conditionals in JSX

--- resource-a/src/App.js
+++ resource-a/src/App.js
@@ -1,29 +1,26 @@
 import React from 'react';
 
-const SayWelcome = () => {
-  return React.createElement('div', { className: 'header' }, React.createElement('h1', {}, 'Welcome ;)'));
-};
-
 const App = () => {
   const title = 'Blog Post';
-  const body = 'This is my blog post';
   const comments = [
     { id: 1, text: 'comment 1' },
     { id: 2, text: 'comment 2' },
     { id: 3, text: 'comment 3' }
   ];
 
+  const showComments = true;
+
   return (
     <div>
-      <SayWelcome />
       <h1>{title}</h1>
-      <p>{body}</p>
       <div className='comments'>
-        <ul>
-          {comments.map((comment, index) => (
-            <li key={index}>{comment.text}</li>
-          ))}
-        </ul>
+        {showComments && (
+          <ul>
+            {comments.map((comment, index) => (
+              <li key={index}>{comment.text}</li>
+            ))}
+          </ul>
+        )}
       </div>
     </div>
   );
