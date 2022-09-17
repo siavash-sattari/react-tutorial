2f03e4c44ba8f4d0bcfe2bf8966887f768be46d9
siavash sattari <siavash.sattari.75@gmail.com>
2022-09-17 07:28:38 -0700

    dynamic values & lIsts in JSX

--- resource-a/src/App.js
+++ resource-a/src/App.js
@@ -1,7 +1,32 @@
 import React from 'react';
 
+const SayWelcome = () => {
+  return React.createElement('div', { className: 'header' }, React.createElement('h1', {}, 'Welcome ;)'));
+};
+
 const App = () => {
-  return <div>App</div>;
+  const title = 'Blog Post';
+  const body = 'This is my blog post';
+  const comments = [
+    { id: 1, text: 'comment 1' },
+    { id: 2, text: 'comment 2' },
+    { id: 3, text: 'comment 3' }
+  ];
+
+  return (
+    <div>
+      <SayWelcome />
+      <h1>{title}</h1>
+      <p>{body}</p>
+      <div className='comments'>
+        <ul>
+          {comments.map((comment, index) => (
+            <li key={index}>{comment.text}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default App;
