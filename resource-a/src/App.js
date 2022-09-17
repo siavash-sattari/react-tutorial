da49de02dddc8c29b6a6f78ad3108c07804c6467
siavash sattari <siavash.sattari.75@gmail.com>
2022-09-17 08:45:32 -0700

    feedbackList component & global state

--- resource-a/src/App.js
+++ resource-a/src/App.js
@@ -1,11 +1,18 @@
+import { useState } from 'react';
+
 import Header from './components/Header';
+import FeedbackList from './components/FeedbackList';
+import FeedbackData from './data/FeedbackData';
+
+// ==============================================================
 
 const App = () => {
+  const [feedback, setFeedback] = useState(FeedbackData);
   return (
     <>
       <Header />
       <div className='container'>
-        <h1>My App</h1>
+        <FeedbackList feedback={feedback} />
       </div>
     </>
   );
