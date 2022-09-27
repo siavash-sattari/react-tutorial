977b81f9282aa903e38d17a011c08d4d590500f9
siavash sattari <siavash.sattari.75@gmail.com>
2022-09-27 04:36:26 -0700

    Add feedback via API

--- resource-a/src/context/FeedbackContext.js
+++ resource-a/src/context/FeedbackContext.js
@@ -1,5 +1,4 @@
 import { useState, useEffect, createContext, useContext } from 'react';
-import { v4 as uuidv4 } from 'uuid';
 const FeedbackContext = createContext();
 
 export const FeedbackProvider = ({ children }) => {
@@ -16,7 +15,7 @@ export const FeedbackProvider = ({ children }) => {
 
   // Fetch feedback
   const fetchFeedback = async () => {
-    const response = await fetch('http://localhost:5000/feedback?_sort=id&_order=desc');
+    const response = await fetch(`/feedback?_sort=id&_order=desc`);
     const data = await response.json();
     setFeedback(data);
     setIsLoading(false);
@@ -28,9 +27,18 @@ export const FeedbackProvider = ({ children }) => {
     }
   };
 
-  const addFeedback = newFeedback => {
-    newFeedback.id = uuidv4();
-    setFeedback([newFeedback, ...feedback]);
+  const addFeedback = async newFeedback => {
+    const response = await fetch('/feedback', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newFeedback)
+    });
+
+    const data = await response.json();
+
+    setFeedback([data, ...feedback]);
   };
 
   const editFeedback = item => {
