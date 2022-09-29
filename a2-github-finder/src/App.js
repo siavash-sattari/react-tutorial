2fe880593d469c76babe4d585138eadb732b17a1
siavash sattari <siavash.sattari.75@gmail.com>
2022-09-29 02:13:54 -0700

    Footer component

--- a2-github-finder/src/App.js
+++ a2-github-finder/src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Footer from './layout/Footer';
 import Navbar from './layout/Navbar';
 
 const App = () => {
@@ -7,6 +8,7 @@ const App = () => {
       <div className='flex flex-col justify-between h-screen'>
         <Navbar />
         <main>Content</main>
+        <Footer />
       </div>
     </Router>
   );
