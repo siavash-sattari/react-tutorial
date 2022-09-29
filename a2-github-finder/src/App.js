af834e527a8a8956d34d4ad67562c388b2b06787
siavash sattari <siavash.sattari.75@gmail.com>
2022-09-29 04:05:56 -0700

    Setup github context

--- a2-github-finder/src/App.js
+++ a2-github-finder/src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { GithubProvider } from './context/GithubContext'
 
 import Footer from './layout/Footer';
 import Navbar from './layout/Navbar';
@@ -9,19 +10,21 @@ import NotFound from './pages/NotFound';
 
 const App = () => {
   return (
-    <Router>
-      <div className='flex flex-col justify-between h-screen'>
-        <Navbar />
-        <main className='container mx-auto px-3 pb-12'>
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/about' element={<About />} />
-            <Route path='*' element={<NotFound />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <GithubProvider>
+      <Router>
+        <div className='flex flex-col justify-between h-screen'>
+          <Navbar />
+          <main className='container mx-auto px-3 pb-12'>
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/about' element={<About />} />
+              <Route path='*' element={<NotFound />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </Router>
+    </GithubProvider>
   );
 };
 
