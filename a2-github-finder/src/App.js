b01ccdf92f66cc447832bd58c3e33ab2748adb4f
siavash sattari <siavash.sattari.75@gmail.com>
2022-09-29 02:24:01 -0700

    Pages and routes

--- a2-github-finder/src/App.js
+++ a2-github-finder/src/App.js
@@ -1,13 +1,24 @@
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import Footer from './layout/Footer';
 import Navbar from './layout/Navbar';
+
+import Home from './pages/Home';
+import About from './pages/About';
+import NotFound from './pages/NotFound';
 
 const App = () => {
   return (
     <Router>
       <div className='flex flex-col justify-between h-screen'>
         <Navbar />
-        <main>Content</main>
+        <main className='container mx-auto px-3 pb-12'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='*' element={<NotFound />} />
+          </Routes>
+        </main>
         <Footer />
       </div>
     </Router>
