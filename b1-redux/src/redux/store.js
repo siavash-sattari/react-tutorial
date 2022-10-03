c018e2abec7bb7da45034b538fd7697671d65716
siavash sattari <siavash.sattari.75@gmail.com>
2022-10-03 15:50:38 -0700

    save the Redux store in persistent storage

--- b1-redux/src/redux/store.js
+++ b1-redux/src/redux/store.js
@@ -1,6 +1,18 @@
 import { legacy_createStore as createStore } from 'redux';
 import reducers from './reducers/rootReducer';
 
-const store = createStore(reducers);
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
 
-export default store;
+const persistConfig = {
+    key: 'root',
+    storage,
+    // whitelist: ['count'] => only count will be persisted
+    // blacklist: ['product'] => product will not be persisted
+}
+
+const persistedReducer = persistReducer(persistConfig, reducers)
+const store = createStore(persistedReducer)
+const persistor = persistStore(store)
+
+export {store, persistor}
