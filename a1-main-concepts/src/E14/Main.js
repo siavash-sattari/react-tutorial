fd27cfd4d54a83429c25c2c063c3516d1bf522bf
siavash sattari <siavash.sattari.dev@gmail.com>
2022-12-04 14:41:52 -0800

    a1e14 - note app mini project

---
+++ a1-main-concepts/src/E14/Main.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import NoteApp from './NoteApp';
+import './Main.css';
+
+export default class Main extends Component {
+  render() {
+    return <NoteApp />;
+  }
+}
