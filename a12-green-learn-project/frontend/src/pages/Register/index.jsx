327631df8245afaedc764a5662c696b0a9a9e837
siavash sattari <siavash.sattari.dev@gmail.com>
2022-12-06 14:32:09 -0800

    implement login/register page

---
+++ a12-green-learn-project/frontend/src/pages/Register/index.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Footer from '../../components/Footer';
+import Navbar from '../../components/Navbar';
+import Topbar from '../../components/Topbar';
+import './Register.css';
+
+const Register = () => {
+  return (
+    <>
+      <Topbar />
+      <Navbar />
+      <section className='login-register'>
+        <div className='login register-form'>
+          <span className='login__title'>ساخت حساب کاربری</span>
+          <span className='login__subtitle'>خوشحالیم قراره به جمع ما بپیوندی</span>
+          <div className='login__new-member'>
+            <span className='login__new-member-text'>قبلا ثبت‌نام کرده‌اید؟ </span>
+            <Link className='login__new-member-link' to='/login'>
+              وارد شوید
+            </Link>
+          </div>
+          <form action='#' className='login-form'>
+            <div className='login-form__username'>
+              <input className='login-form__username-input' type='text' placeholder='نام کاربری' />
+              <i className='login-form__username-icon fa fa-user'></i>
+            </div>
+            <div className='login-form__password'>
+              <input className='login-form__password-input' type='text' placeholder='آدرس ایمیل' />
+              <i className='login-form__password-icon fa fa-envelope'></i>
+            </div>
+            <div className='login-form__password'>
+              <input className='login-form__password-input' type='text' placeholder='رمز عبور' />
+              <i className='login-form__password-icon fa fa-lock-open'></i>
+            </div>
+            <button className='login-form__btn' type='submit'>
+              <i className='login-form__btn-icon fa fa-user-plus'></i>
+              <span className='login-form__btn-text'>عضویت</span>
+            </button>
+          </form>
+          <div className='login__des'>
+            <span className='login__des-title'>سلام کاربر محترم:</span>
+            <ul className='login__des-list'>
+              <li className='login__des-item'>لطفا از مرورگر های مطمئن و بروز مانند گوگل کروم و فایرفاکس استفاده کنید.</li>
+              <li className='login__des-item'>ما هرگز اطلاعات محرمانه شمارا از طریق ایمیل درخواست نمیکنیم.</li>
+              <li className='login__des-item'>لطفا کلمه عبور خود را در فواصل زمانی کوتاه تغییر دهید.</li>
+            </ul>
+          </div>
+        </div>
+      </section>
+      <Footer />
+    </>
+  );
+};
+
+export default Register;
--- a12-green-learn-project/frontend/src/routes.js
+++ a12-green-learn-project/frontend/src/routes.js
@@ -3,13 +3,17 @@ import CourseInfo from './pages/CourseInfo';
 import Category from './pages/Category';
 import ArticleInfo from './pages/ArticleInfo';
 import Courses from './pages/Courses';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 const routes = [
   { path: '/', element: <Index /> },
   { path: '/course-info/:courseName', element: <CourseInfo /> },
   { path: '/category-info/:categoryName', element: <Category /> },
   { path: '/article-info/:articleName', element: <ArticleInfo /> },
-  { path: '/courses', element: <Courses /> }
+  { path: '/courses', element: <Courses /> },
+  { path: '/login', element: <Login /> },
+  { path: '/register', element: <Register /> }
 ];
 
 export default routes;
